2ea8e078f859e7d1b0059a63303f9ee035c1287c
Jay Goel <dev@jaygoel.com>
2012-03-12 22:06:45 -0400

    update tabs to spaces

--- ResumeHTML.py
+++ ResumeHTML.py
@@ -3,19 +3,19 @@ import json
 from genshi.template import MarkupTemplate
 
 class ResumeHTML(object):
-	def __init__(self, resume):
-		self.resume = resume
+   def __init__(self, resume):
+      self.resume = resume
 
-	def create_output(self):
-		templ = MarkupTemplate('<h1>Hello, $fname</h1>')
-		stream = templ.generate(**resume)
-		print stream
-		print stream.render('xhtml')
-		print 'hello world'
+   def create_output(self):
+      templ = MarkupTemplate('<h1>Hello, $fname</h1>')
+      stream = templ.generate(**resume)
+      print stream
+      print stream.render('xhtml')
+      print 'hello world'
 
 if __name__ == '__main__':
-	fd = open('resume.json')
-	resume = json.load(fd)
+   fd = open('resume.json')
+   resume = json.load(fd)
 
-	r = ResumeHTML(resume)
-	r.create_output()
+   r = ResumeHTML(resume)
+   r.create_output()
