116784b406eb6177b8b727d8d0e1ae26a20d9223
Jay Goel <dev@jaygoel.com>
2012-03-17 16:33:11 -0400

    update templates for hresume-specific structure

--- ResumeHTML.py
+++ ResumeHTML.py
@@ -1,15 +1,30 @@
 import sys
 import json
+from pprint import pprint
+from copy import deepcopy
 from genshi.template import MarkupTemplate, TemplateLoader
 
 class ResumeHTML(object):
    def __init__(self, resume):
       self.resume = resume
 
+   def convert_to_hresume(self):
+      # TODO: htmlencode 
+      # TODO: markdown to html
+      hresume = deepcopy(self.resume)
+
+      # TODO: Convert json to hresume-specific formats (ie iso date format)
+      hresume['hresume'] = deepcopy(self.resume)
+      return hresume
+
    def create_output(self):
+      r = self.convert_to_hresume()
+
       loader = TemplateLoader('.')
       templ = loader.load('ResumeHTMLTemplate.html') 
-      stream = templ.generate(**resume)
+      stream = templ.generate(**r)
+
+      # TODO: output to file? method param for this?
       print stream.render('xhtml')
 
 if __name__ == '__main__':
