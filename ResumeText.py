57cf13118dae417c55e0d4708d73b753ba47188b
kevlened <boyettel@gmail.com>
2012-05-08 11:07:48 -0400

    fixed

--- ResumeText.py
+++ ResumeText.py
@@ -4,7 +4,6 @@ from pprint import pprint
 from copy import deepcopy
 from genshi.template import NewTextTemplate, TemplateLoader
 
-<<<<<<< HEAD
 def wrap(text, width=80):
     # Thanks Dominick Saputo! http://code.activestate.com/recipes/148061-one-liner-word-wrap-function/
     lines = []
@@ -23,13 +22,10 @@ def wrap(text, width=80):
         lines.append(' '.join(line))
     return '\n'.join(lines)
 
-=======
->>>>>>> f91433fdac69a165113ef80d9ce10cbe7ef9fc68
 class ResumeText(object):
    def __init__(self, resume):
       self.resume = resume
 
-<<<<<<< HEAD
    def preprocess_resume(self):
       self.resume['tresume'] = self.resume
       
@@ -57,30 +53,9 @@ class ResumeText(object):
    def postprocess_resume(self):
       self.generatedResume = wrap(self.generatedResume,resume['max_columns'])  
    
-=======
-   def convert_to_tresume(self):
-      tresume = deepcopy(self.resume)
-
-      tresume['tresume'] = deepcopy(self.resume)
-      return tresume
-
-   def create_output(self):
-      r = self.convert_to_tresume()
-
-      templ = NewTextTemplate('''{% include ResumeTextTemplate.txt %}''')
-      stream = templ.generate(**r)
-
-      # TODO: output to file? method param for this?
-      print stream.render('text')
-
->>>>>>> f91433fdac69a165113ef80d9ce10cbe7ef9fc68
 if __name__ == '__main__':
    fd = open('resume.json')
    resume = json.load(fd)
 
    r = ResumeText(resume)
-<<<<<<< HEAD
    r.create_output("out.txt")
-=======
-   r.create_output()
->>>>>>> f91433fdac69a165113ef80d9ce10cbe7ef9fc68
