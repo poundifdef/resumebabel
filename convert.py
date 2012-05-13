f2fbff37f39eda65c4b96e6d579b2bf3c5ba2ee6
Jay Goel <dev@jaygoel.com>
2012-05-13 13:21:35 -0400

    rename main convert program

--- convert.py
+++ convert.py
@@ -1,11 +1,11 @@
 import sys
 import json
 
-from resumebabel.converters.resume_to_txt import ResumeText
+from resumebabel.converters.txt_converter import TXTConverter
    
 if __name__ == '__main__':
    fd = open(sys.argv[1])
    resume = json.load(fd)
 
-   r = ResumeText(resume)
+   r = TXTConverter(resume)
    r.create_output("out.txt")
--- run.sh
+++ run.sh
@@ -1,2 +1,1 @@
-#PYTHONPATH=/home/jay/github/resumebabel python resumebabel/converters/resume_to_txt.py resume.json
-PYTHONPATH=/home/jay/github/resumebabel python resume_to_txt.py resume.json
+PYTHONPATH=/home/jay/github/resumebabel python convert.py resume.json
