fb2dd3673c6d3568efd1aa3eff1cdb22d4fd1fb7
Jay Goel <dev@jaygoel.com>
2012-09-24 01:29:01 -0400

    oops, forgot to add resumebabel module

---
+++ resumebabel/resumebabel.py
@@ -0,0 +1,27 @@
+import collections
+import json
+import os
+import sys
+
+class ResumeBabel:
+    def __init__(self, resume_json):
+        self.resume = json.loads(resume_json, object_pairs_hook=collections.OrderedDict)
+
+    def export_resume(self, output_format):
+        """Export raw data of converted resume"""
+        module_name = ('resumebabel.converters.' + output_format + '_converter.' +
+                       output_format.upper() + 'Converter')
+
+        resume_converter_class = self._get_class(module_name)
+
+        r = resume_converter_class(self.resume)
+        return r.process_resume()
+
+    # http://stackoverflow.com/a/452981/3788
+    def _get_class(self, kls):
+        parts = kls.split('.')
+        module = ".".join(parts[:-1])
+        m = __import__(module)
+        for comp in parts[1:]:
+            m = getattr(m, comp)
+        return m
