be1c430fcf3989f4508ce4a5bf7d469b853e6310
Jay Goel <dev@jaygoel.com>
2012-09-30 11:32:57 -0400

    cleanup html code

--- resumebabel/converters/html_converter.py
+++ resumebabel/converters/html_converter.py
@@ -10,9 +10,8 @@ class HTMLConverter(ConverterParent):
         self.preprocessed = {}
         super(HTMLConverter, self).__init__(resume)
 
-    #def preprocess_resume(self):
-        # TODO: htmlencode
-        # TODO: markdown to html
+    def preprocess_resume(self):
+        # TODO: markdown to html for more that description?
         self.preprocessed['resume'] = deepcopy(self.resume)
 
         for experience_title, experiences in self.preprocessed['resume']['experiences'].iteritems():
@@ -20,15 +19,9 @@ class HTMLConverter(ConverterParent):
                 if experience.get('description'):
                     experience['description'] = markdown2.markdown(experience['description'])
 
-        #self.preprocessed['hresume'] = deepcopy(self.resume)
-
-        # TODO: Convert json to hresume-specific formats (ie iso date format)
-        # self.preprocessed['hresume'] = CONVERT_HRESUME_FORMATS_HERE()
-
     def do_conversion(self):
         template_filename = self.get_resource('html_template.html')
 
         env = Environment(loader=PackageLoader('resumebabel', 'resources'))
         template = env.get_template('html_template.html')
         self.generated_resume = template.render(**self.preprocessed)
-        # TODO: output to file? method param for this?
